58936df01109e089b51873dc6e6176b14a1c4f1d
James Bond <jb108681@gmail.com>
2019-11-07 00:41:37 -0800

    se

--- Networking/Client.java
+++ Networking/Client.java
@@ -1,10 +1,14 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Server Address: ");
+        String add = in.nextLine();
         try {
-            Socket socket = new Socket("localhost", 6666);
+            Socket socket = new Socket(add, 6666);
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
             out.writeUTF("I am client");
             out.flush();
