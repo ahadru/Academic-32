dad06a185205642d136d6c987193f00bbea5952c
ahadru <ahadsheikh1999@gmail.com>
2019-11-14 17:15:28 +0600

    good

--- Networking/FileClient.java
+++ Networking/FileClient.java
@@ -5,32 +5,31 @@ import java.net.Socket;
 
 public class FileClient {
 	
-	private Socket s;
+	private Socket socket;
 	
 	public FileClient(String host, int port, String file) {
 		try {
-			s = new Socket(host, port);
+			socket = new Socket(host, port);
 			sendFile(file);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}		
 	}
-	
 	public void sendFile(String file) throws IOException {
-		DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-		FileInputStream fis = new FileInputStream(file);
+		DataOutputStream dataOutputStream = new DataOutputStream(s.getOutputStream());
+		FileInputStream fileInputStream = new FileInputStream(file);
 		byte[] buffer = new byte[4096];
 		
-		while (fis.read(buffer) > 0) {
-			dos.write(buffer);
+		while (fileInputStream.read(buffer) > 0) {
+			dataOutputStream.write(buffer);
 		}
 		
-		fis.close();
-		dos.close();	
+		fileInputStream.close();
+		dataOutputStream.close();	
 	}
 	
 	public static void main(String[] args) {
-		FileClient fc = new FileClient("localhost", 1988, "cat.jpeg");
+		FileClient fileClient = new FileClient("localhost", 1988, "cat.jpeg");
 	}
 
 }
--- Networking/FileServer.java
+++ Networking/FileServer.java
@@ -6,11 +6,11 @@ import java.net.Socket;
 
 public class FileServer extends Thread {
 	
-	private ServerSocket ss;
+	private ServerSocket serverSocket;
 	
 	public FileServer(int port) {
 		try {
-			ss = new ServerSocket(port);
+			serverSocket = new ServerSocket(port);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -19,7 +19,7 @@ public class FileServer extends Thread {
 	public void run() {
 		while (true) {
 			try {
-				Socket clientSock = ss.accept();
+				Socket clientSock = serverSocket.accept();
 				saveFile(clientSock);
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -28,28 +28,28 @@ public class FileServer extends Thread {
 	}
 
 	private void saveFile(Socket clientSock) throws IOException {
-		DataInputStream dis = new DataInputStream(clientSock.getInputStream());
-		FileOutputStream fos = new FileOutputStream("testfile.jpeg");
+		DataInputStream dataInputStream = new DataInputStream(clientSock.getInputStream());
+		FileOutputStream fileOutputStream = new FileOutputStream("testfile.jpeg");
 		byte[] buffer = new byte[4096];
 		
 		int filesize = 15123; // Send file size in separate msg
 		int read = 0;
 		int totalRead = 0;
 		int remaining = filesize;
-		while((read = dis.read(buffer, 0, Math.min(buffer.length, remaining))) > 0) {
+		while((read = dataInputStream.read(buffer, 0, Math.min(buffer.length, remaining))) > 0) {
 			totalRead += read;
 			remaining -= read;
 			System.out.println("read " + totalRead + " bytes.");
-			fos.write(buffer, 0, read);
+			fileOutputStream.write(buffer, 0, read);
 		}
 		
-		fos.close();
-		dis.close();
+		fileOutputStream.close();
+		dataInputStream.close();
 	}
 	
 	public static void main(String[] args) {
-		FileServer fs = new FileServer(1988);
-		fs.start();
+		FileServer fileServer = new FileServer(1988);
+		fileServer.start();
 	}
 
 }
