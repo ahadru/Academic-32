9be86e2c36c65adcd3c5a4785e3dcdc44c35cb35
James Bond <jb108681@gmail.com>
2019-12-29 11:44:36 +0600

    29-12

--- OS/bankers_algorithm.cpp
+++ OS/bankers_algorithm.cpp
@@ -1,94 +1,64 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-    int alloc[10][10], max[10][10], avail[10], n, m, need[10][10], i, j, k, finish[10], work[10];
-    int SSeq[10];
+// Banker's Algorithm 
+#include <stdio.h> 
+int main() 
+{ 
+	// P0, P1, P2, P3, P4 are the Process names here 
 
-    cout << "Enter no. of processes:";
-    cin >> n;
-    cout << "Enter no. of resources:";`
-    cin >> m;
-    // cout<<"Enter allocation matrix:";
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < m; j++)
-        {
-            cout << "\nEnter no. of instances of " << j + 1 << "allocated to " << i + 1 << ":";
-            cin >> alloc[i][j];
-        }
-    }
-    // cout<<"\nEnter max matrix";
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < m; j++)
-        {
-            cout << "\nEnter maximum instances of" << j + 1 << "allocated to" << i + 1 << ":";
-            cin >> max[i][j];
-        }
-    }
+	int n, m, i, j, k; 
+	n = 5; // Number of processes 
+	m = 3; // Number of resources 
+	int alloc[5][3] = { { 0, 1, 0 }, // P0 // Allocation Matrix 
+						{ 2, 0, 0 }, // P1 
+						{ 3, 0, 2 }, // P2 
+						{ 2, 1, 1 }, // P3 
+						{ 0, 0, 2 } }; // P4 
 
-    for (i = 0; i < m; i++)
-    {
-        cout << "\nEnter available instances of" << i + 1;
-        cin >> avail[i];
-    }
-    //calculate need matrix
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < m; j++)
-        {
-            need[i][j] = max[i][j] - alloc[i][j];
-        }
-    }
-    cout << "\nAllocation matrix is:";
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < m; j++)
-        {
-            cout << alloc[i][j] << "\t";
-        }
-        cout << endl;
-    }
-    cout << "\nMax matrix is:";
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < m; j++)
-        {
-            cout << max[i][j] << "\t";
-        }
-        cout << endl;
-    }
-    cout << "\nNeed matrix is:";
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < m; j++)
-        {
-            cout << need[i][j] << "\t";
-        }
-        cout << endl;
-    }
-    //initialize work and finish
-    for (i = 0; i < n; i++)
-        work[i] = avail[i];
-    k = 0;
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < m; j++)
-        {
-            if (finish[i] == 0 && need[i][j] <= work[j])
-            {
-                SSeq[k] = i + 1;
-                k++;
-                work[i] = work[i] + alloc[i][j];
-            }
-        }
-    }
-    cout << "safe sequence is:";
-    for (i = 0; i < k; i++)
-        cout << SSeq[i] << ",";
-    if (k == n)
-        cout << "\nThe system is safe.\n";
-    else
-        cout << "\nDeadlock occurence!!\n";
-}
+	int max[5][3] = { { 7, 5, 3 }, // P0 // MAX Matrix 
+					{ 3, 2, 2 }, // P1 
+					{ 9, 0, 2 }, // P2 
+					{ 2, 2, 2 }, // P3 
+					{ 4, 3, 3 } }; // P4 
+
+	int avail[3] = { 3, 3, 2 }; // Available Resources 
+
+	int f[n], ans[n], ind = 0; 
+	for (k = 0; k < n; k++) { 
+		f[k] = 0; 
+	} 
+	int need[n][m]; 
+	for (i = 0; i < n; i++) { 
+		for (j = 0; j < m; j++) 
+			need[i][j] = max[i][j] - alloc[i][j]; 
+	} 
+	int y = 0; 
+	for (k = 0; k < 5; k++) { 
+		for (i = 0; i < n; i++) { 
+			if (f[i] == 0) { 
+
+				int flag = 0; 
+				for (j = 0; j < m; j++) { 
+					if (need[i][j] > avail[j]){ 
+						flag = 1; 
+						break; 
+					} 
+				} 
+
+				if (flag == 0) { 
+					ans[ind++] = i; 
+					for (y = 0; y < m; y++) 
+						avail[y] += alloc[i][y]; 
+					f[i] = 1; 
+				} 
+			} 
+		} 
+	} 
+
+	printf("Following is the SAFE Sequence\n"); 
+	for (i = 0; i < n - 1; i++) 
+		printf(" P%d ->", ans[i]); 
+	printf(" P%d", ans[n - 1]); 
+
+	return (0); 
+
+	// This code is contributed by Deep Baldha (CandyZack) 
+} 
