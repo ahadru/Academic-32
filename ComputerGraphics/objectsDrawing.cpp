a080e1036bb317d7a90e305e39a426e1693ba9b6
ahadru <ahadsheikh1999@gmail.com>
2020-01-31 01:50:52 +0600

    dda added

---
+++ ComputerGraphics/objectsDrawing.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <graphics.h>
+using namespace std;
+
+int main(){
+    int gd=DETECT, gm=DETECT;
+    initgraph(&gm,&gd,NULL);
+
+    line(50,30,400,30);
+    circle(250, 100, 50);
+    rectangle(100, 100, 400, 400);
+    delay(10000);
+    closegraph();
+
+    return 0;
+}
