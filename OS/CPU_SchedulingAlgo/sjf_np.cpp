b70d337f15aff7170c7f49b8a6afab9cbf9f9975
ahadru <ahadsheikh1999@gmail.com>
2020-03-04 02:26:40 +0600

    OS Final

--- OS/CPU_SchedulingAlgo/sjf_np.cpp
+++ OS/CPU_SchedulingAlgo/sjf_np.cpp
@@ -1,21 +1,130 @@
-    #include <bits/stdc++.h>
-    #define M0(a) memset(a,0,sizeof a)
-    #define M1(a) memset(a,1,sizeof a)
-    #define M1_(a) memset(a,-1,sizeof a)
-    #define D(v) cerr<<#v<<": "<<v<<endl
-    #define D2(a,b) cerr<<#a<<": "<<a<<","<<#b<<": "<<b<<endl
-    #define For(i,n) for( __typeof(n)i = 0 ; i < n ; i++)
-    #define For1(i,n) for( __typeof(n)i = 1 ; i <= n ; i++)
-    #define ForIT(i,n) for(__typeof(n.begin())i = n.begin();i!=n.end();i++)
-    #define M100 10000000
-    #define M10 1000000
-    #define M 100000
-    #define MS 10000
-    using namespace std;
-    
-    int main(){
-        //freopen("in.txt","r",stdin);
-        //freopen("out.txt","w",stdout);
-    
-        return 0;
+///Shortest Job First (Preemptive)
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ck(XX) cout<<XX<<endl
+#define Fin freopen("in.txt", "r", stdin)
+
+struct process{
+    int id;
+    int bt;//burst time
+    int art;//arriving time
+    int rt;//remaining time
+
+    int st;//start time
+    int et;//end time
+    int wt;//waiting time
+    int tat;//turn around time
+
+    process (){st = -1;}
+};
+
+bool operator < (process p1, process p2)
+{
+    return p1.art<p2.art;
+}
+
+int n; // total number of process
+process p[10000];
+
+void inputProcess()
+{
+    cout<<"Enter process id, burst time and arriving time\n";
+    for(int i=0; i<n; i++)
+    {
+        cin>>p[i].id>>p[i].bt>>p[i].art;
+        p[i].rt = p[i].bt;
     }
+    return;
+}
+
+void calculateWT()
+{
+    for(int i=0; i<n; i++)
+    {
+        p[i].wt = p[i].et - p[i].art - p[i].bt;
+    }
+    return;
+}
+
+void calculateTAT()
+{
+    for(int i=0; i<n; i++)
+    {
+        p[i].tat = p[i].bt + p[i].wt;
+    }
+    return;
+}
+
+void SJFP()
+{
+    int totalWT = 0;
+    int totalTAT = 0;
+
+    int completed = 0;
+    int time=0;
+    int processIndex = -1;
+
+
+    while(completed < n)
+    {
+        int mn = 1000000;
+        for(int i=0; p[i].art <= time && i<n; i++)
+        {
+            if(p[i].rt>0 && mn>p[i].rt)
+            {
+                processIndex = i;
+                mn = p[i].rt;
+            }
+        }
+
+        if(processIndex == -1)
+        {
+            time++;
+            continue;
+        }
+
+        if(p[processIndex].st == -1) p[processIndex].st = time;
+        time++;
+        p[processIndex].rt--;
+        if(p[processIndex].rt == 0)
+        {
+            completed++;
+            p[processIndex].et = time;
+            processIndex = -1;
+        }
+    }
+    calculateWT();
+    calculateTAT();
+
+    for(int i=0; i<n; i++)
+    {
+        totalWT = totalWT + p[i].wt;
+        totalTAT = totalTAT + p[i].tat;
+    }
+
+    cout<<"\nID\tBT\tWT\tTAT\n";
+    for(int i=0; i<n; i++)
+    {
+        cout<<p[i].id<<"\t"<<p[i].bt<<"\t"<<p[i].wt<<"\t"<<p[i].tat<<endl;
+    }
+
+    cout<<"Average Waiting Time : "<<(double)totalWT/n<<endl;
+    cout<<"Average Turn Around Time : "<<(double)totalTAT/n<<endl;
+    return;
+
+}
+
+int main()
+{
+    //Fin;
+    cout<<"Enter total number of processes : ";
+    cin>>n;
+
+    inputProcess();
+    sort(p, p+n);
+
+    SJFP();
+
+    return 0;
+}
--- OS/bankers_algorithm.cpp
+++ OS/bankers_algorithm.cpp
@@ -1,177 +1,133 @@
-#include <bits/stdc++.h>
-#include <iostream>
-#define MAX 20
-using namespace std;
- 
-class bankers
-{
-    private:
-        int al[MAX][MAX],m[MAX][MAX],n[MAX][MAX],avail[MAX];
-        int nop,nor,k,result[MAX],pnum,work[MAX],finish[MAX];
- 
-    public:
-        bankers();
-        void input();
-        void method();
-        int search(int);
-        void display();
-};
- 
-bankers::bankers()
-{
-    k=0;
-    for(int i=0;i<MAX;i++)
-    {
-        for(int j=0;j<MAX;j++)
-        {
-            al[i][j]=0;
-            m[i][j]=0;
-            n[i][j]=0;
-        }
-        avail[i]=0;
-        result[i]=0;
-        finish[i]=0;
-    }
-}
- 
-void bankers::input()
-{
-    int i,j;
-    cout << "Enter the number of processes:";
-    cin >> nop;
-    cout << "Enter the number of resources:";
-    cin >> nor;
-    cout << "Enter the allocated resources for each process: " << endl;
-    for(i=0;i<nop;i++)
-    {
-        cout<<"\nProcess "<<i;
-        for(j=0;j<nor;j++)
-        {
-            cout<<"\nResource "<<j<<":";
-            cin>>al[i][j];
-        }
-    }
-    cout<<"Enter the maximum resources that are needed for each process: "<<endl;
-    for(i=0;i<nop;i++)
-    {
-        cout<<"\nProcess "<<i;
-        for(j=0;j<nor;j++)
-        {
-            cout<<"\nResouce "<<j<<":";
-            cin>>m[i][j];
-            n[i][j]=m[i][j]-al[i][j];
-        }
-    }
-    cout << "Enter the currently available resources in the system: ";
-    for(j=0;j<nor;j++)
-    {
-        cout<<"Resource "<<j<<":";
-        cin>>avail[j];
-        work[j]=-1;
-    }
-    for(i=0;i<nop;i++)
-        finish[i]=0;
-}
- 
-void bankers::method()
-{
-    int i=0,j,flag;
-    while(1)
-    {
-        if(finish[i]==0)
-        {
-            pnum =search(i);
-            if(pnum!=-1)
-            {
-                result[k++]=i;
-                finish[i]=1;
-                for(j=0;j<nor;j++)
-                {
-                    avail[j]=avail[j]+al[i][j];
-                }
-            }
-        }
-        i++;
-        if(i==nop)
-        {
-            flag=0;
-            for(j=0;j<nor;j++)
-                if(avail[j]!=work[j])
- 
-            flag=1;
-            for(j=0;j<nor;j++)
-                work[j]=avail[j];
- 
-            if(flag==0)
-                break;
-            else
-                i=0;
-        }
-    }
-}
- 
-int bankers::search(int i)
-{
-    int j;
-    for(j=0;j<nor;j++)
-        if(n[i][j]>avail[j])
-            return -1;
-    return 0;
-}
- 
-void bankers::display()
-{
-    int i,j;
-    cout<<endl<<"OUTPUT:";
-    cout<<endl<<"========";
-    cout<<endl<<"PROCESS\t     ALLOTED\t     MAXIMUM\t     NEED";
-    for(i=0;i<nop;i++)
-    {
-        cout<<"\nP"<<i+1<<"\t     ";
-        for(j=0;j<nor;j++)
-        {
-            cout<<al[i][j]<<"  ";
-        }
-        cout<<"\t     ";
-        for (j=0;j<nor;j++)
-        {
-            cout<<m[i][j]<<"  ";
-        }
-        cout<<"\t     ";
-        for(j=0;j<nor;j++ )
-        {
-            cout<<n[i][j]<<"  ";
-        }
-    }
-    cout<<"\nThe sequence of the safe processes are: \n";
-    for(i=0;i<k;i++)
-    {
-        int temp = result[i]+1 ;
-        cout<<"P"<<temp<<" ";
-    }
-    cout<<"\nThe sequence of unsafe processes are: \n";
-    int flg=0;
-    for (i=0;i<nop;i++)
-    {
-        if(finish[i]==0)
-        {
-        flg=1;
-        }
-        cout<<"P"<<i<<" ";
-    }
-    cout<<endl<<"RESULT:";
-    cout<<endl<<"=======";
-    if(flg==1)
-        cout<<endl<<"The system is not in safe state and deadlock may occur!!";
-    else
-        cout<<endl<<"The system is in safe state and deadlock will not occur!!";
-}
- 
-int main()
-{
-    cout<<" DEADLOCK BANKER’S ALGORITHM "<<endl;
-    bankers B;
-    B.input ( );
-    B.method ( );
-    B.display ( );
-}
+// C++ program to illustrate Banker's Algorithm 
+#include<iostream> 
+using namespace std; 
+
+// Number of processes 
+const int P = 5; 
+
+// Number of resources 
+const int R = 3; 
+
+// Function to find the need of each process 
+void calculateNeed(int need[P][R], int maxm[P][R], 
+				int allot[P][R]) 
+{ 
+	// Calculating Need of each P 
+	for (int i = 0 ; i < P ; i++) 
+		for (int j = 0 ; j < R ; j++) 
+
+			// Need of instance = maxm instance - 
+			//				 allocated instance 
+			need[i][j] = maxm[i][j] - allot[i][j]; 
+} 
+
+// Function to find the system is in safe state or not 
+bool isSafe(int processes[], int avail[], int maxm[][R], 
+			int allot[][R]) 
+{ 
+	int need[P][R]; 
+
+	// Function to calculate need matrix 
+	calculateNeed(need, maxm, allot); 
+
+	// Mark all processes as infinish 
+	bool finish[P] = {0}; 
+
+	// To store safe sequence 
+	int safeSeq[P]; 
+
+	// Make a copy of available resources 
+	int work[R]; 
+	for (int i = 0; i < R ; i++) 
+		work[i] = avail[i]; 
+
+	// While all processes are not finished 
+	// or system is not in safe state. 
+	int count = 0; 
+	while (count < P) 
+	{ 
+		// Find a process which is not finish and 
+		// whose needs can be satisfied with current 
+		// work[] resources. 
+		bool found = false; 
+		for (int p = 0; p < P; p++) 
+		{ 
+			// First check if a process is finished, 
+			// if no, go for next condition 
+			if (finish[p] == 0) 
+			{ 
+				// Check if for all resources of 
+				// current P need is less 
+				// than work 
+				int j; 
+				for (j = 0; j < R; j++) 
+					if (need[p][j] > work[j]) 
+						break; 
+
+				// If all needs of p were satisfied. 
+				if (j == R) 
+				{ 
+					// Add the allocated resources of 
+					// current P to the available/work 
+					// resources i.e.free the resources 
+					for (int k = 0 ; k < R ; k++) 
+						work[k] += allot[p][k]; 
+
+					// Add this process to safe sequence. 
+					safeSeq[count++] = p; 
+
+					// Mark this p as finished 
+					finish[p] = 1; 
+
+					found = true; 
+				} 
+			} 
+		} 
+
+		// If we could not find a next process in safe 
+		// sequence. 
+		if (found == false) 
+		{ 
+			cout << "System is not in safe state"; 
+			return false; 
+		} 
+	} 
+
+	// If system is in safe state then 
+	// safe sequence will be as below 
+	cout << "System is in safe state.\nSafe"
+		" sequence is: "; 
+	for (int i = 0; i < P ; i++) 
+		cout << safeSeq[i] << " "; 
+
+	return true; 
+} 
+
+// Driver code 
+int main() 
+{ 
+	int processes[] = {0, 1, 2, 3, 4}; 
+
+	// Available instances of resources 
+	int avail[] = {3, 3, 2}; 
+
+	// Maximum R that can be allocated 
+	// to processes 
+	int maxm[][R] = {{7, 5, 3}, 
+					{3, 2, 2}, 
+					{9, 0, 2}, 
+					{2, 2, 2}, 
+					{4, 3, 3}}; 
+
+	// Resources allocated to processes 
+	int allot[][R] = {{0, 1, 0}, 
+					{2, 0, 0}, 
+					{3, 0, 2}, 
+					{2, 1, 1}, 
+					{0, 0, 2}}; 
+
+	// Check system is in safe state or not 
+	isSafe(processes, avail, maxm, allot); 
+
+	return 0; 
+} 
